94626b43b4ed36d34fa1cc007c6eba2bda6a2ea7
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-06 12:36:09 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ivanova";
+            string surname = "Anna";
+            string exchange;
+            Console.WriteLine($" Ваше имя {name}. Ваша фамилия {surname}.");
+
+            exchange = name;
+            name = surname;
+            surname = exchange;
+            Console.WriteLine($" Ваше имя {name}. Ваша фамилия {surname}");
+        }
+
+    }
+}
